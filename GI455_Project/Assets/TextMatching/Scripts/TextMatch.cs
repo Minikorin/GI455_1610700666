c1c808ddd75c4494c36462712d7bd308a72de4be
LAPTOP-NK2QECBA\tonkett <ahtiwat.chat@bumail.net>
2021-01-27 16:43:13 +0700

    Send Assignment Week 1

---
+++ GI455_Project/Assets/TextMatching/Scripts/TextMatch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextMatch : MonoBehaviour
+{
+    public Text inputText;
+    public Text resultText;
+
+    public void Start()
+    {
+        resultText.text = "status: ";
+    }
+
+    public void CheckText()
+    {
+        if (inputText.text == "Nathamon")
+        {
+            resultText.text = inputText.text + " is found";
+
+        }
+        else if (inputText.text == "Mean")
+        {
+            resultText.text = inputText.text + " is found";
+
+        }
+        else if (inputText.text == "GI455")
+        {
+            resultText.text = inputText.text + " is found";
+
+        }
+        else if (inputText.text == "Network")
+        {
+            resultText.text = inputText.text + " is found";
+
+        }
+        else if (inputText.text == "Cat")
+        {
+            resultText.text = inputText.text + " is found";
+
+        }
+        else if (inputText.text == "Computer")
+        {
+            resultText.text = inputText.text + " is found";
+
+        }
+        else if (inputText.text != "Nathamon" || inputText.text != "Mean"|| inputText.text != "GI455"||
+        inputText.text != "Network"||inputText.text != "Cat"||inputText.text != "Computer")
+        {
+
+            resultText.text = inputText.text + "  is not found";
+        }
+        
+       
+    }
+}
