6f44d5fb420791a9fea8c8604b680e3d42ba9da7
LAPTOP-NK2QECBA\tonkett <ahtiwat.chat@bumail.net>
2021-01-30 17:31:41 +0700

    In class week2-Programchat

---
+++ GI455_Project/Assets/ProgramChat/Scripts/WebsocketConnection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using Random = UnityEngine.Random;
+
+namespace ProgramChat
+{
+    public class WebsocketConnection : MonoBehaviour
+    {
+
+        private WebSocket websocket;
+      
+        void Start()
+        {
+            websocket = new WebSocket("ws://127.0.0.1:25500/");
+            websocket.OnMessage += OnMessage;
+            websocket.Connect();
+            
+        }
+
+     
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                websocket.Send("Number :" + Random.Range(0,999));
+            }
+        }
+
+        public void OnDestroy()
+        {
+            if (websocket != null)
+            {
+                websocket.Close();
+            }
+        }
+
+        public void OnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            Debug.Log("Message from server :" +messageEventArgs.Data);
+        }
+    }
+}
+
